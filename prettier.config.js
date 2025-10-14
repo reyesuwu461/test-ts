e1615b9ddbd459e93aefbabdd8ca484aaaa52656
reyesuwu461 <samuelreyescastro456@gmail.com>
2025-10-14 11:28:37 -0500

    prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,1 +1,11 @@
-UWU
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
+const config = {
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  importOrder: ["^react", "<THIRD_PARTY_MODULES>", "^[.]"],
+  tailwindFunctions: ["clsx"],
+};
+
+export default config;
